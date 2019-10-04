53945f441e748c3fff64c699441e6ad98f608448
maple3142 <kirby741852963@gmail.com>
2019-10-04 08:13:48 +0800

    fix

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: yarn start
-aria2c: aria2c --conf-path=aria2c.conf --listen-port=$PORT
+web: aria2c --conf-path=aria2c.conf& && yarn start
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const express = require('express')
 const httpsrv = require('httpsrv')
+const request = require('request')
 
 const PORT = process.env.PORT || 1234
-httpsrv({
-	basedir: __dirname + '/downloads'
-}).listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+const app = express()
+app.get('/jsonrpc', (req, res) => {
+	req.pipe(request('https://localhost:6800/jsonrpc')).pipe(res)
+})
+app.use(
+	httpsrv({
+		basedir: __dirname + '/downloads'
+	})
+)
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
