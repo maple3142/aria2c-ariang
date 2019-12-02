63742b65e6a1f05b68c001ef60ad555e8433bd10
maple3142 <kirby741852963@gmail.com>
2019-12-02 19:50:36 +0800

    add home page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const httpsrv = require('httpsrv')
 const request = require('request')
+const fs = require('fs')
+const ENCODED_SECRET = btoa(
+	/rpc-secret=(.*)/.exec(fs.readFileSync('aria2c.conf', 'utf-8'))[1]
+)
 
 const PORT = process.env.PORT || 1234
 const app = express()
@@ -13,5 +17,13 @@ app.use(
 		basedir: __dirname + '/downloads'
 	})
 )
-app.use(express.static(__dirname + '/ariang'))
+app.use('/ariang', express.static(__dirname + '/ariang'))
+app.get('/', (req, res) => {
+	const host = req.headers.host
+	const url = `https://${host}/ariang/#!/settings/rpc/set/https/${host}/443/jsonrpc/${ENCODED_SECRET}`
+	res.send(`
+<a href="${url}">AriaNg Panel</a>
+<a href="/downloads/">Downloaded files</a>
+`)
+})
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
