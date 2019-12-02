e1edc3f97e40fba2b760a977e0a9562c36f0f181
maple3142 <kirby741852963@gmail.com>
2019-12-02 19:55:25 +0800

    fix base64

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const httpsrv = require('httpsrv')
 const request = require('request')
 const fs = require('fs')
-const ENCODED_SECRET = btoa(
+const ENCODED_SECRET = Buffer.from(
 	/rpc-secret=(.*)/.exec(fs.readFileSync('aria2c.conf', 'utf-8'))[1]
-)
+).toString('base64')
 
 const PORT = process.env.PORT || 1234
 const app = express()
