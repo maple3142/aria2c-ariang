a5f8c232613332c4af75cdf8cfac082063f8700e
maple3142 <kirby741852963@gmail.com>
2020-01-02 20:44:17 +0800

    add trackers

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	chmod +x on-complete.sh
 fi
 
-sed -i "s/SECRET/$ARIA2C_SECRET/" aria2c.conf
+tracker_list() {
+	curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat
+}
+echo "bt-tracker=$tracker_list" >> aria2c.conf
+echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
 aria2c --conf-path=aria2c.conf&
 yarn start
