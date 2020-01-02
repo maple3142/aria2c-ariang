5eda0e69a2ff7e8041643d66d9eaae294c095728
maple3142 <kirby741852963@gmail.com>
2020-01-02 20:23:09 +0800

    update on-complete script

--- on-complete.sh
+++ on-complete.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "aria2 parameters: $1 $2 $3"
-rclone -v --config="rclone.conf" copy downloads "DRIVE:$RCLONE_DESTINATION" --exclude="*.aria2" 2>&1
+rclone -v --config="rclone.conf" copy $3 "DRIVE:$RCLONE_DESTINATION" 2>&1
