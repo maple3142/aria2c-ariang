d4572ba478730823f755595f42a366a4cd6aa02a
maple3142 <kirby741852963@gmail.com>
2020-02-10 21:12:23 +0800

    prevent idle when uploading

--- on-complete.sh
+++ on-complete.sh
@@ -4,8 +4,12 @@ filePath=$3
 relativePath=${filepath#./downloads/}
 topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
 
+echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
+
 if [[ $2 -eq 1 ]]; then # single file
 	rclone -v --config="rclone.conf" copy "$3" "DRIVE:$RCLONE_DESTINATION" 2>&1
 elif [[ $2 -gt 1 ]]; then # multiple file
 	rclone -v --config="rclone.conf" copy "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
 fi
+
+echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
