e27cd80cd10cd7027fae7fe5386e49390255c85e
fanweiya <fanweiya@users.noreply.github.com>
2020-03-10 10:03:11 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -21,11 +21,11 @@ echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
 if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="rclone.conf" --no-traverse copy "$3" "$RCLONE_DESTINATION" 2>&1
-	rclone -v --config="rclone.conf" --delete-empty-src-dirs --no-traverse move "$3" "$RCLONE_DESTINATION_2" 2>&1	
+	#rclone -v --config="rclone.conf" --no-traverse copy "$3" "$RCLONE_DESTINATION" 2>&1
+	rclone -v --config="rclone.conf" --delete-empty-src-dirs --no-traverse move "$3" "$RCLONE_DESTINATION" 2>&1	
 elif [[ $2 -gt 1 ]]; then # multiple file
-	rclone -v --config="rclone.conf" --no-traverse copy "$topPath" "$RCLONE_DESTINATION/${relativePath%%/*}"
-	rclone -v --config="rclone.conf" --delete-empty-src-dirs --no-traverse move "$topPath" "$RCLONE_DESTINATION_2/${relativePath%%/*}"
+	#rclone -v --config="rclone.conf" --no-traverse copy "$topPath" "$RCLONE_DESTINATION/${relativePath%%/*}"
+	rclone -v --config="rclone.conf" --delete-empty-src-dirs --no-traverse move "$topPath" "$RCLONE_DESTINATION/${relativePath%%/*}"
 fi
 
 echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
