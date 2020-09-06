9112145a37a6c3e55a937d85074830673a3592a6
maple3142 <kirby741852963@gmail.com>
2020-09-07 01:42:08 +0800

    improve aria2c & update rclone

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
-unzip -q rclone-v1.51.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
+wget -q https://downloads.rclone.org/v1.53.0/rclone-v1.53.0-linux-amd64.zip
+unzip -q rclone-v1.53.0-linux-amd64.zip
+export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
 
 # Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
+wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
+tar xf aria2-1.35.0-static-linux-amd64.tar.gz
+export PATH=$PWD:$PATH
 
 # Create download folder
 mkdir -p downloads
