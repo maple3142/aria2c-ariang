0b3f0241c12bb0827e9dc5a5fec61f7eba14879a
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2021-05-09 10:58:35 +0800

    Update rclone and aria2c

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.53.0/rclone-v1.53.0-linux-amd64.zip
-unzip -q rclone-v1.53.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
+wget -q https://downloads.rclone.org/v1.55.1/rclone-v1.55.1-linux-amd64.zip
+unzip -q rclone-v1.55.1-linux-amd64.zip
+export PATH=$PWD/rclone-v1.55.1-linux-amd64:$PATH
 
 # Install aria2c static binary
-wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
+wget -q https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.35.0_2021.02.19/aria2-1.35.0-static-linux-amd64.tar.gz
 tar xf aria2-1.35.0-static-linux-amd64.tar.gz
 export PATH=$PWD:$PATH
 
